a178b7c8c5a4b194a188ad1a6c46bfc76bfe746a
Denis Ouma <dennisouma38@gmail.com>
2023-04-17 17:53:57 +0300

    Updating the name and car cost in the state

--- src/componets/CarForm.jsx
+++ src/componets/CarForm.jsx
@@ -1,9 +1,53 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { changeCost, changeName } from "../store";
 export const CarForm = () => {
+	const dispatch = useDispatch();
+	const { name, cost } = useSelector((state) => {
+		return {
+			name: state.form.name,
+			cost: state.form.cost,
+		};
+	});
+
+	const handleNameChange = (e) => {
+		dispatch(changeName(e.target.value));
+	};
+	const handleCostChange = (e) => {
+		dispatch(changeCost(Number(e.target.value))) || 0;
+	};
+	console.log(name, cost);
 	return (
-		<div>
-			<h1>Welcome to my Form application</h1>
+		<div className="car-form panel">
+			<h4 className="subtitle is-3">Add Car</h4>
+			<form>
+				<div className="field-group">
+					<div className="field">
+						<label htmlFor="name" className="label">
+							Name
+						</label>
+						<input
+							type="text"
+							value={name}
+							className="input is-expanded"
+							onChange={handleNameChange}
+						/>
+					</div>
+				</div>
+				<div className="field-group">
+					<div className="field">
+						<label htmlFor="name" className="label">
+							Cost
+						</label>
+						<input
+							type="number"
+							value={cost}
+							className="input is-expanded"
+							onChange={handleCostChange}
+						/>
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
